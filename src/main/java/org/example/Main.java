f4b638a088f1f169f5ebec86e3492c38b96d533b
SamaraMoreira <128256951+SamaraMoreira@users.noreply.github.com>
2024-01-03 10:48:24 -0300

    Seller entity class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,15 @@
 package org.example;
 
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(21,"Vini","amoasamara@gmail.com",new Date(),5000.0,obj);
+        System.out.println( seller);
     }
 }
