d2ccceba61fb93d9f171a078af56ca774e36e029
SamaraMoreira <128256951+SamaraMoreira@users.noreply.github.com>
2024-01-06 19:01:48 -0300

    Department implementation

--- src/main/java/impl/DepartmentDaoJDBC.java
+++ src/main/java/impl/DepartmentDaoJDBC.java
@@ -1,14 +1,15 @@
 package impl;
 
 import dao.DepartmentDao;
+import db.DB;
 import db.DbException;
 import entities.Department;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
 
@@ -17,29 +18,118 @@ public class DepartmentDaoJDBC implements DepartmentDao {
     public DepartmentDaoJDBC(Connection conn) {
         this.conn = conn;
     }
-
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("Id"));
+        dep.setName(rs.getString("Name"));
+        return dep;
+    }
     @Override
     public void insert(Department obj) {
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement("INSERT INTO department (name) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
+            st.setString(1, obj.getName());
+            int rowsAffected = st.executeUpdate();
 
+            if (rowsAffected > 0){
+                ResultSet rs = st.getGeneratedKeys();
+                if ((rs.next())) {
+                    int id = rs.getInt(1);
+                    obj.setId(id);
+                }
+                DB.closeResultSet(rs);
+            }else {
+                throw new DbException("Nenhuma linha foi alterada");
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
     public void update(Department obj) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("UPDATE department\n" +
+                    "SET Name = ? WHERE Id = ?");
+            st.setString(1,obj.getName());
+            st.setInt(2, obj.getId());
+            st.executeUpdate();
+        }catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }finally {
+            DB.closeStatement(st);
+        }
+
 
     }
 
     @Override
     public void deleteById(Integer id) {
-
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement("   DELETE FROM department\n" +
+                    "    WHERE Id = ?");
+            st.setInt(1, id);
+            st.executeUpdate();
+        }catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
     public Department findById(Integer id) {
-         return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement("SELECT * From department WHERE Id = ?");
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if (rs.next()) {
+                Department dep = instantiateDepartment(rs);
+                return dep;
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+
     }
 
     @Override
     public List<Department> findAll() {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement("SELECT * FROM department");
+            rs = st.executeQuery();
+            List<Department> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+            while (rs.next()) {
+                Department dep = map.get(rs.getInt("Id"));
+                if (dep == null) {
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("Id"), dep);
+                }
+                Department obj = instantiateDepartment(rs);
+                list.add(obj);
+            }
+            return list;
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
 package org.example;
 
 import dao.DaoFactory;
+import dao.DepartmentDao;
 import dao.SellerDao;
 import entities.Department;
 import entities.Seller;
+import impl.DepartmentDaoJDBC;
 
 import javax.swing.*;
 import java.util.Date;
@@ -11,37 +13,63 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        SellerDao sellerDao = DaoFactory.createSellerDao();
-        System.out.println(" === TEST 1: seller findByid ===");
-        Seller seller = sellerDao.findById(1);
-        System.out.println( seller);
-        System.out.println(" === TEST 2: seller findByDepartment ===");
-        Department department = new Department(2,null);
-        List<Seller> list = sellerDao.findByDepartment(department);
-        for (Seller obj:list) {
+          SellerDao sellerDao = DaoFactory.createSellerDao();
+          DepartmentDao DepartmentDao = DaoFactory.createDepartmentDao();
+
+//        System.out.println(" === TEST 1: seller findByid ===");
+//        Seller seller = sellerDao.findById(1);
+//        System.out.println( seller);
+//        System.out.println(" === TEST 2: seller findByDepartment ===");
+//        Department department = new Department(2,null);
+//        List<Seller> list = sellerDao.findByDepartment(department);
+//        for (Seller obj:list) {
+//            System.out.println(obj);
+//        }
+//        System.out.println(" === TEST 3: seller findAll ===");
+//        list = sellerDao.findAll();
+//        for (Seller obj:list) {
+//            System.out.println(obj);
+//        }
+//        System.out.println(" === TEST 4: seller insert ===");
+//          Seller newSeller = new Seller(null, "Vini", "greg@gmail.com", new Date(), 4000.0, department);
+//          sellerDao.insert(newSeller);
+//        System.out.println(" Inserted! New id = " + newSeller.getId());
+//
+//        System.out.println(" === TEST 5: seller update===");
+//        seller = sellerDao.findById(2);
+//        seller.setName("Samara");
+//        sellerDao.update(seller);
+//        System.out.println(" Update completed!");
+//
+//        System.out.println(" === TEST 6: seller delete===");
+//        var id1  = JOptionPane.showInputDialog("Entre com o id para exclusão: ");
+//        sellerDao.deleteById(Integer.valueOf(id1));
+//        System.out.println(" Delete completed!");
+
+
+//        System.out.println(" === TEST 1: departmen insert ===");
+//        Department newDep = new Department(null, "Teste");
+//        DepartmentDao.insert(newDep);
+//        System.out.println(" Inserted! New id = " + newDep.getId());
+//
+//        System.out.println(" === TEST 2: departmen update===");
+//        Department dep = DepartmentDao.findById(1);
+//        dep.setName("vini");
+//        DepartmentDao.update(dep);
+//        System.out.println(" Update completed!");
+
+//        System.out.println(" === TEST 3: department findByid ===");
+//        Department dep = DepartmentDao.findById(1);
+//        System.out.println(dep);
+//        System.out.println(" === TEST 4: Department delete===");
+//        var id1  = JOptionPane.showInputDialog("Entre com o id para exclusão: ");
+//        DepartmentDao.deleteById(Integer.valueOf(id1));
+//        System.out.println(" Delete completed!");
+        System.out.println(" === TEST 5: departmen findAll ===");
+        List<Department> list = DepartmentDao.findAll();
+        for (Department obj:list) {
             System.out.println(obj);
         }
-        System.out.println(" === TEST 3: seller findAll ===");
-        list = sellerDao.findAll();
-        for (Seller obj:list) {
-            System.out.println(obj);
-        }
-        System.out.println(" === TEST 4: seller insert ===");
-        Seller newSeller = new Seller(null, "Vini", "greg@gmail.com", new Date(), 4000.0, department);
-        sellerDao.insert(newSeller);
-        System.out.println(" Inserted! New id = " + newSeller.getId());
-
-        System.out.println(" === TEST 5: seller update===");
-        seller = sellerDao.findById(2);
-        seller.setName("Samara");
-        sellerDao.update(seller);
-        System.out.println(" Update completed!");
-
-        System.out.println(" === TEST 6: seller delete===");
-        var id1  = JOptionPane.showInputDialog("Entre com o id para exclusão: ");
-        sellerDao.deleteById(Integer.valueOf(id1));
-        System.out.println(" Delete completed!");
-
 
     }
 }
